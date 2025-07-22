2ff8998b841d05aa4df7499bcf09a615e9be0cbe
maxi2594 <maxi2594@hotmail.com>
2025-07-21 21:09:06 -0300

    cors configurationSource added

--- src/main/java/com/tuempresa/reservas/SecurityConfig.java
+++ src/main/java/com/tuempresa/reservas/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+                .cors(cors->cors.configurationSource(configurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**",
                                 "/swagger-ui/**",
